f5fee678814c126ec251999b64ace4ba2866b0e4
Krzysztof <s22463@pjwstk.edu.pl>
2024-05-29 15:37:13 +0200

    Task2 - fix

--- ZAD1/src/main/java/org/example/Main.java
+++ ZAD1/src/main/java/org/example/Main.java
@@ -2,48 +2,56 @@ package org.example;
 
 import lombok.NonNull;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
-        @NonNull int[] input = getNumbersFromUser();
-        @NonNull int[] occurrences = sumUpOccurrences(input);
 
-        for (int i = 0; i <= 6; i++) {
-            if (occurrences[13 - i] > 0) {
-                for (int j = 0; j < occurrences[i]; j++) {
-                    System.out.println(i + " " + (13 - i));
+    public static void main(String[] args) {
+       @NonNull Map<Integer, Long> occurrences =
+                getNumbersFromUser()
+                        .stream()
+                        .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
+
+        displayPairsThatSumUpTo13(occurrences);
+    }
+
+    private static void displayPairsThatSumUpTo13(@NonNull Map<Integer, Long> occurrences) {
+        Set<Integer> keys = occurrences.keySet()
+                .stream()
+                .sorted()
+                .collect(Collectors.toCollection(LinkedHashSet::new)); //O(1)
+
+        Iterator<Integer> iterator = keys.iterator();
+
+        int key;
+        int complement;
+        while (iterator.hasNext()) {
+            key = iterator.next();
+            complement = 13 - key;
+
+            if (keys.contains(complement)) {
+                for (int j = 0; j < occurrences.get(key); j++) {
+                    System.out.println(key + " " + complement);
                 }
             }
+
+            iterator.remove();
         }
     }
 
 
     @NonNull
-    private static int[] sumUpOccurrences(@NonNull int[] input) {
-        int[] occurrences = new int[14];
-
-        try {
-            for (int number : input) {
-                occurrences[number]++;
-            }
-            return occurrences;
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Only digits from 0 to 13, please");
-            throw e;
-        }
-    }
-
-    @NonNull
-    private static int[] getNumbersFromUser() {
-        System.out.println("I need positive integers separated by space character: ");
+    private static List<Integer> getNumbersFromUser() {
+        System.out.println("I need integers separated by space character: ");
         String[] input = new Scanner(System.in).nextLine().split(" ");
 
         try {
             return Arrays.stream(input)
                     .mapToInt(Integer::parseInt)
-                    .toArray();
+                    .boxed()
+                    .collect(Collectors.toList());
+
         } catch (NumberFormatException exception) {
             System.out.println("I operate only on integers, sorry.");
             throw exception;
